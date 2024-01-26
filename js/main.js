eddcaa18a25ff4c57369b963a8ad0f8d8a547b2b
dLswStudy <devLswStudy@gmail.com>
2024-01-26 17:14:55 +0900

    Visual Section / 13~15. 순차적 애니메이션

--- js/main.js
+++ js/main.js
@@ -14,3 +14,40 @@ searchInputEl.addEventListener('blur', function() {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 })
+
+const badgeEl = document.querySelector('header .badges');
+
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener('scroll', _.throttle(function () {
+    // 페이지 스크롤 위치가 500px이 넘으면.
+    if (window.scrollY > 500) {
+        // Badge 요소 숨기기!
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        })
+
+        // 페이지 스크롤 위치가 500px이 넘지 않으면.
+    } else {
+        // Badge 요소 보이기!
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        })
+    }
+}, 300));
+
+
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+// 나타날 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach(function (fadeEl, index) {
+    // 각 요소들을 순서대로(delay) 보여지게 함!
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7,
+        opacity: 1
+    })
+})
